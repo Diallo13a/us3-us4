e97427899c2d904b162b2277b8bd78a18f0a3a1b
Diallo13a <62150957+Diallo13a@users.noreply.github.com>
2020-04-17 09:04:16 +0000

    Add files via upload

---
+++ listequestions.php
@@ -0,0 +1,9 @@
+<div class="tete">
+<form action="" method="post">
+<p>
+Nbre de questions/jeu
+<input type="number" style="width:50px" name="nbq">
+<button type="button"  >OK</button>
+</p>
+</form>
+</div>
